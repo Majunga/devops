4f963c163231df5ad3494ed2efd24a345f4b87a7
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2021-01-19 12:27:04 +0000

    Updated do kuber version

--- src/digitalocean/resource.tf
+++ src/digitalocean/resource.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = var.ClusterName
   region  = var.Region
-  version = "1.18.8-do.1"
+  version = "1.19.3-do.3"
 
   node_pool {
     name       = "nodes"
