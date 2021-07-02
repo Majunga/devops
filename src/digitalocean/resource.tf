50fcf0f85b30e6dc1d5851d1346e25244972ce95
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2021-07-02 21:06:32 +0100

    Updated version of the digitalocean terrform for Kubernetes

--- src/digitalocean/resource.tf
+++ src/digitalocean/resource.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = var.ClusterName
   region  = var.Region
-  version = "1.21.2-do.1"
+  version = "~> 2.0"
 
   node_pool {
     name       = "nodes"
